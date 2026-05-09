6693e4fa1abe9ea97cc93f6fc79f408f4f92f5e1
Rezwoan <frezwoan@gmail.com>
2026-05-09 17:17:29 +0600

    chore: Firebase and Gradle setup

--- app/src/main/java/com/reprush/app/RepRushApp.kt
+++ app/src/main/java/com/reprush/app/RepRushApp.kt
@@ -1,7 +1,22 @@
 package com.reprush.app
 
 import android.app.Application
+import com.google.firebase.Firebase
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.PersistentCacheSettings
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.firestoreSettings
+import com.google.firebase.firestore.persistentCacheSettings
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class RepRushApp : Application()
+class RepRushApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+        val settings = firestoreSettings {
+            setLocalCacheSettings(persistentCacheSettings {})
+        }
+        Firebase.firestore.firestoreSettings = settings
+    }
+}
