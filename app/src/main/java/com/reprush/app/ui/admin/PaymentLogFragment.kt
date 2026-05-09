55adae17a7bbee5925a84fdfccd62ea78e266682
Rezwoan <frezwoan@gmail.com>
2026-05-09 22:45:19 +0600

    ui: Added empty fragments and corrosponding kt files

---
+++ app/src/main/java/com/reprush/app/ui/admin/PaymentLogFragment.kt
@@ -0,0 +1,6 @@
+package com.reprush.app.ui.admin
+
+import androidx.fragment.app.Fragment
+import com.reprush.app.R
+
+class PaymentLogFragment : Fragment(R.layout.fragment_payment_log)
---
+++ app/src/main/java/com/reprush/app/ui/member/ProfileFragment.kt
@@ -0,0 +1,6 @@
+package com.reprush.app.ui.member
+
+import androidx.fragment.app.Fragment
+import com.reprush.app.R
+
+class ProfileFragment : Fragment(R.layout.fragment_profile)
